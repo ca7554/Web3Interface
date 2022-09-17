7a65efbff4a5c79e5b884a7f11229e92f33b1792
ca7554 <a.christopher7554@gmail.com>
2022-09-17 03:18:58 -0500

    Added example on how to use to main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import crypto.blockChain.PolygonBlockchain;
+import crypto.entity.decetralizedExchange.QuickSwap;
+import crypto.web3Gateway.PolygonGateway.PocketNetworkPolygonHttpGateway;
+import quickUtils.QuickUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+            Example for printing price of WETH on QuickSwap on the polygon blockchain using a decentralized rpc endpoint.
+         */
+
+        //Creates a polygon Web3GateWay from PocketNetwork's decentralized rpc endpoint.
+        PocketNetworkPolygonHttpGateway pocketNetworkPolygonHttpGateway = PocketNetworkPolygonHttpGateway.getInstance();
+
+        //Creates a PolygonBlockchain and passes the Web3GateWay to the PolygonBlockchain.
+        PolygonBlockchain polygonBlockchain = new PolygonBlockchain(pocketNetworkPolygonHttpGateway);
+
+        //Creates a QuickSwap object and passes the PolygonBlockchain to the QuickSwap object.
+        QuickSwap quickSwap = new QuickSwap(polygonBlockchain);
+
+        //Addresses of assets contracts (case-sensitive)
+        String polygonUsdcAddress = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
+        String polygonWethAddress = "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
+
+        //Gets the price on QuickSwap
+        String price = quickSwap.getPriceOfCryptoCurrencyFromLiquidityPair(polygonUsdcAddress, polygonWethAddress, 1);
+
+        //Prints price
+        QuickUtils.println(price);
+
+        //Shutdown Web3J instance
+        polygonBlockchain.shutDown();
     }
 }
